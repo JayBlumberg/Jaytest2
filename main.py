09a0edc1268f8f764f738c1483d40a9c0f79d70f
JayBlumberg <31857945+JayBlumberg@users.noreply.github.com>
2017-09-13 18:28:54 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,3 @@
 #!/usr/bin/env python
-# -*- coding: UTF-8 -*-
-print ("Content-Type: text/plain;charset=utf-8")
-print ("Hello World!")
+print "Content-Type: text/plain;charset=utf-8"
+print "Hello World!"
