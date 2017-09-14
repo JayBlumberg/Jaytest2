99caa6ff55656c83bdbcc5fb38ef1b772d98e361
JayBlumberg <31857945+JayBlumberg@users.noreply.github.com>
2017-09-13 21:22:31 -0500

    Update app.py

--- app.py
+++ app.py
@@ -9,6 +9,8 @@ if __name__ == '__main__':
     from wsgiref.simple_server import make_server
 
     httpd = make_server('localhost', 5555, wsgi_app)
+
+    httpd.serve_forever()
     httpd = make_server('localhost', 5555, wsgi_app)
     httpd = make_server('localhost', 5555, "dkldj")
     httpd.serve_forever()
