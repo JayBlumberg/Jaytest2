547102d13b123b49fea456d525731c67c9b13406
JayBlumberg <31857945+JayBlumberg@users.noreply.github.com>
2017-09-13 21:32:12 -0500

    Update app.py

--- app.py
+++ app.py
@@ -2,8 +2,10 @@ def wsgi_app(environ, start_response):
     status = '200 OK'
     response_headers = [('Content-type', 'text/plain')]
     start_response(status, response_headers)
-       response_body = 'Hello World app'
+    response_body = 'Hello World app'
     yield response_body.encode()
+
+
 
 if __name__ == '__main__':
     from wsgiref.simple_server import make_server
