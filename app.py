0c10aad1bd4ebbde0a7dd7b2e4df3249d64bfebf
JayBlumberg <31857945+JayBlumberg@users.noreply.github.com>
2017-09-15 12:35:41 -0500

    Update app.py

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ def wsgi_app(environ, start_response):
     response_headers = [('Content-type', 'text/plain')]
     start_response(status, response_headers)
     
+    import time
     today = date.today()
     
     text = 'hello world app.py - ' + today
