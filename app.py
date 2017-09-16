8a0a55b97f9f30a6997f5e4101c5b5638061f7de
JayBlumberg <31857945+JayBlumberg@users.noreply.github.com>
2017-09-16 09:48:50 -0400

    Update app.py

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ def wsgi_app(environ, start_response):
     status = '200 OK'
     response_headers = [('Content-type', 'text/plain')]
     start_response(status, response_headers)
-    response_body = 'Hello World app'
+    response_body = 'Hello World app-1'
     yield response_body.encode()
 
 
